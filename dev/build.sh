b2b616dfcc30885faff30757d61006967889cb38
Christian Nikolov <christian@newton.codes>
2018-01-18 04:13:20 +0200

    Fix build script version

--- dev/build.sh
+++ dev/build.sh
@@ -2,6 +2,6 @@
 
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
-cd ${dir}/.. && docker build -t newtoncodes/node .
-cd ${dir}/.. && docker build -t newtoncodes/node:8.x .
-cd ${dir}/.. && docker build -t newtoncodes/node:9.x .
+cd ${dir}/../9.x && docker build -t newtoncodes/node .
+cd ${dir}/../8.x && docker build -t newtoncodes/node:8.x .
+cd ${dir}/../9.x && docker build -t newtoncodes/node:9.x .
