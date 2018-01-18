b478a0c7159350e3791398e6c230a4458b69d0ba
Christian Nikolov <christian@newton.codes>
2018-01-18 04:12:35 +0200

    Split by versions

--- dev/build.sh
+++ dev/build.sh
@@ -3,4 +3,5 @@
 dir=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
 
 cd ${dir}/.. && docker build -t newtoncodes/node .
-cd ${dir}/.. && docker build -t newtoncodes/node:9.4 .
+cd ${dir}/.. && docker build -t newtoncodes/node:8.x .
+cd ${dir}/.. && docker build -t newtoncodes/node:9.x .
--- dev/push.sh
+++ dev/push.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 docker push newtoncodes/node
-docker push newtoncodes/node:9.4
+docker push newtoncodes/node:8.x
+docker push newtoncodes/node:9.x
+
